9ab09606414c08af8a8dda0602202f48178ddc9c
isuru cumaranathunga <isurucuma@gmail.com>
2024-05-02 14:05:24 +0530

    calculator done

---
+++ ch5/main.go
@@ -0,0 +1,33 @@
+package main
+
+import "errors"
+
+var operations = map[string]func(int, int) (int, error){
+	"+": func(a, b int) (int, error) { return a + b, nil },
+	"-": func(a, b int) (int, error) { return a - b, nil },
+	"*": func(a, b int) (int, error) { return a * b, nil },
+	"/": func(a, b int) (int, error) {
+		if b == 0 {
+			return 0, errors.New("division by zero")
+		}
+		return a / b, nil
+	},
+}
+
+func calculate(a int, b int, op string) (int, error) {
+	if operation, ok := operations[op]; ok {
+		return operation(a, b)
+	}
+	return 0, errors.New("operation not found")
+}
+
+func main() {
+	a, b := 10, 0
+	op := "/"
+	result, err := calculate(a, b, op)
+	if err != nil {
+		println("Error:", err.Error())
+	} else {
+		println("Result:", result)
+	}
+}
