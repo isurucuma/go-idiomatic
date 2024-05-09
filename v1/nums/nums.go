cd74347d3e39e47c02b0b107cf031fd9abac8ee7
isuru <isurucuma@gmail.com>
2024-05-09 23:12:50 +0530

    chapter 10 exercise 2 Go doc added

--- v1/nums/nums.go
+++ v1/nums/nums.go
@@ -1,5 +1,11 @@
 package nums
 
+//# Add <- this is a comment header
+//
+// This is a link to get more info [mathsisfun].
+//
+// [mathsisfun]: https://www.mathsisfun.com/numbers/addition.html
+// Add returns the sum of two integers
 func Add(a, b int) int {
 	return a + b
 }
