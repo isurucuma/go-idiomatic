f52ff11ba78967e7a7388badffe14bb15911b197
isuru <isurucuma@gmail.com>
2024-05-09 23:37:54 +0530

    new directory created for the v2

--- v2/nums/nums.go
+++ v2/nums/nums.go
@@ -1,11 +1,19 @@
 package nums
 
+import (
+	"golang.org/x/exp/constraints"
+)
+
+type Number interface {
+	constraints.Integer | constraints.Float
+}
+
 // # Add <- this is a comment header
 //
 // This is a link to get more info [mathsisfun].
 //
 // [mathsisfun]: https://www.mathsisfun.com/numbers/addition.html
 // Add returns the sum of two integers
-func Add(a, b int) int {
+func Add[T Number](a, b T) T {
 	return a + b
 }
